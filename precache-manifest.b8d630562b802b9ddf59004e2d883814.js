0f49cf943867420a6f4c108c7278df8c14cb8861
itelo filho <itelofilho@gmail.com>
2019-07-23 19:37:11 -0300

    Updates

---
+++ precache-manifest.b8d630562b802b9ddf59004e2d883814.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dd4b1b6182ccf04aead248380c665e8",
+    "url": "/react-canvas-snake-game/index.html"
+  },
+  {
+    "revision": "635026af18318b32e0f6",
+    "url": "/react-canvas-snake-game/static/css/main.19393e92.chunk.css"
+  },
+  {
+    "revision": "02510454a8d1b2d4a899",
+    "url": "/react-canvas-snake-game/static/js/2.b74c8ea6.chunk.js"
+  },
+  {
+    "revision": "635026af18318b32e0f6",
+    "url": "/react-canvas-snake-game/static/js/main.8f59976e.chunk.js"
+  },
+  {
+    "revision": "e352ab29e84dad2ede75",
+    "url": "/react-canvas-snake-game/static/js/runtime~main.78303c1d.js"
+  }
+]);
